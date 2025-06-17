4c0bcfb68ed394d7a4353ab4215459a28c7c0ff7
Luiz <luizfernandobrogiato73@gmail.com>
2025-06-17 14:29:25 -0300

    Aprendendo sobre laços de repitição: for(início; teste; incremento) {

--- exc2/js/index.js
+++ exc2/js/index.js
@@ -1,5 +1,5 @@
-function verificar() {
 
+function verificar() {
     var data = new Date();
     var ano = data.getFullYear();
     var fano = document.getElementById('txtano');
@@ -7,36 +7,39 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         alert(' [ERRO]Verifique os dados e tente novamente');
     } else {
-        var fsex = document.getElementsByName('radsex');
-        var idade = ano - Number(fano.value);
+        var fsex = document.querySelectorAll('input[name="radsex"]');
+        var idade = fano.value;
+        console.log(idade)
         var genero = '';
         var img = document.createElement('img');
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
+        img.setAttribute('id', 'foto');
+        if (fsex[0].checked == true) {
             genero = 'Homem'
-            if(idade >= 0 && idade < 10) {
-                img.setAttribute('src', '/exec2/img/bebe-masculino')
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bebe-masculino.png');
             } else if (idade < 21) {
-
+                img.setAttribute('src', 'jovem-masculino.png');
             } else if (idade < 50) {
-
+                img.setAttribute('src', 'adulto-masculino.png');
             } else {
-                
-            } 
-          
+                img.setAttribute('src', 'idoso-masculino.png');
+            }
+
         } else if (fsex[1].checked) {
             genero = 'Mulher'
-            if(idade >= 0 && idade < 10) {
-                
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bebe-feminina.png');
             } else if (idade < 21) {
-
+                img.setAttribute('src', 'jovem-feminina.png');
             } else if (idade < 50) {
-
+                img.setAttribute('src', 'adulto-feminina.png');
             } else {
-                
-            }   
+                img.setAttribute('src', 'idoso-feminino.png');
+            }
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `Nascido(a) em ${idade}.`
+        res.appendChild(img);
+       
     }
 
-    }
+}
